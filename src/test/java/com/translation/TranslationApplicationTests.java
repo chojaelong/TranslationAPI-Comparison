6c2b8facd710469d0aff9bfe19c6d097b0a98cd1
JaeHo.Cho <chojaelong@gmail.com>
2023-04-12 18:46:02 +0900

    [2023-04-12][JaeHo.Cho] jenkins 버그 수정

--- src/test/java/com/translation/TranslationApplicationTests.java
+++ src/test/java/com/translation/TranslationApplicationTests.java
@@ -3,7 +3,7 @@ package com.translation;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class TranslationApplicationTests {
 
     @Test
