60ea50c876d70f9f31c70d9bc6b7deabf7131178
JaeHo.Cho <chojaelong@gmail.com>
2023-04-16 21:27:06 +0900

    [2023-04-16][JaeHo.Cho] 파파고 서비스 부분 단위 테스트 추가

--- src/main/java/com/translation/naverapi/domain/TranslateInfo.java
+++ src/main/java/com/translation/naverapi/domain/TranslateInfo.java
@@ -1,8 +1,12 @@
 package com.translation.naverapi.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TranslateInfo {
 
     private Long id;
--- src/test/java/com/translation/naverapi/service/PapagoServiceTest.java
+++ src/test/java/com/translation/naverapi/service/PapagoServiceTest.java
@@ -3,6 +3,7 @@ package com.translation.naverapi.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.translation.naverapi.domain.TranslateInfo;
 import com.translation.naverapi.repository.MemoryPapagoRepository;
+import com.translation.naverapi.repository.PapagoRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
@@ -16,16 +17,21 @@ class PapagoServiceTest {
 
     @Autowired
     PapagoService papagoService;
+    @Autowired
+    MemoryPapagoRepository memoryPapagoRepository;
 
     @AfterEach
     void afterEach() {
-
+        memoryPapagoRepository.clearStore();
     }
 
     @Test
     void translate() throws JsonProcessingException {
-        String word = "자동차";
-        TranslateInfo translateInfo = papagoService.translate(word);
-    }
+        TranslateInfo target = new TranslateInfo(
+                1L, "자동차", "Car", "自動車",
+                "汽车", "xe hơi", "ein Auto", "Voiture");
+        TranslateInfo translateInfo = papagoService.translate("자동차");
 
+        Assertions.assertThat(target).isEqualTo(translateInfo);
+    }
 }
